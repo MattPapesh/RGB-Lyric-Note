b5362a20b7b56fbef39f97481eaaf8fabc2c6f8e
Matthew Papesh <Papesh.m2@student.jths.org>
2021-11-28 21:53:03 -0600

    updated serial communication

--- RGB-AudioAndDisplay.ino
+++ RGB-AudioAndDisplay.ino
@@ -1,95 +1,206 @@
-#include <Encoder.h>
-#include <LCD_interface.h>
+  #include <pgmStrToRAM.h>
+  #include <LCD_Interface.h>
+  #include <SD.h>
+  #include <TMRpcm.h>
 
-#include <SD.h>
-#include <TMRpcm.h>
+  TMRpcm audio;
+  LCD_Interface interface = LCD_Interface();
 
-    ENCODER dial = ENCODER(NULL, NULL, 2);
+  int const AUDIO_CHIP_SELECT = 4, AUDIO_SPEAKER_PIN = 9, AUDIO_DEFAULT_VOLUME = 6;
 
-    TMRpcm audio;
+  // If the slave arduino is sent this value, it knows that the following data is the command
+  char const SEND_SERIAL_ID = (char)58;
+  // If the slave arduino is sent this value, it knows that the entire command has been sent
+  char const SENT_SERIAL_ID = (char)59;
 
-    LCD_INTERFACE manager;
+  namespace RGB
+  {
+    int const num_of_files = 10; 
 
-    folder mainMenu, musicMenu, lightsMenu, modesMenu, brightnessMenu;
-    
-    int const sSize = 9, mSize = 5, bSize = 5;
-    int const audioChipSelect = 4, audioSpeakerPin = 9, audioDefaultVolume = 6;
+    // RGB high variation animations
+    file* stripes = new file("Stripes");
+    file* rainbow = new file("Rainbow");
+    file* custom = new file("Custom");
+    file* polarized = new file("Polarized");
 
-    int const elapsedMillisForDoublePress = 1000;
-    int prevMillis; 
+    // Monochromatic RGB animations
+    file* red = new file("Red");
+    file* green = new file("Green");
+    file* blue = new file("Blue");
+    file* yellow = new file("Yellow");
+    file* cyan = new file("Cyan");
+    file* violet = new file("Violet");
 
-    char* tempAudioFile;
-    char* prevFolderSelected;
+    file* RGB_files[num_of_files] = 
+    {
+      polarized, stripes, rainbow, custom, 
+      red, green, blue, yellow, cyan, violet
+    }; 
 
-    bool disableAudio = false;
+    // Add animation file parameters
+    void fileSetup()
+    {
+      stripes->addParameter("Flow Rate", 10);
+      stripes->addParameter("Red", 20);
+      stripes->addParameter("Green", 30);
+      stripes->addParameter("Blue", 40); 
 
-void setup()
-{ 
-    Serial.begin(9600);
-    
-    char* songs[sSize] = {"Champion","Holiday","StJimmy","Levels","Light","Stars","You","Top","Hold"};
-    char* modes[mSize] = {"Idle Change", "Idle Red", "Idle Green", "Idle Blue", "Stripes"};
-    char* brightness[bSize] = {"Lowest", "Low", "Medium", "High", "Maximum"}; 
+      rainbow->addParameter("Flow Rate", 0);
+      rainbow->addParameter("Resolution", 0);
 
-    mainMenu.folderName = "**RGB Music**";
-    musicMenu.folderName = "[] Music";
-    lightsMenu.folderName = "[] Lights";
-    modesMenu.folderName = "[] Modes";
-    brightnessMenu.folderName = "[] Brightness";
+      custom->addParameter("Red", 0);
+      custom->addParameter("Green", 0);
+      custom->addParameter("Blue", 0);
 
-    mainMenu.createFolder(NULL, 0);
-    musicMenu.createFolder(songs, sSize);
-    lightsMenu.createFolder(NULL, 0);
-    modesMenu.createFolder(modes,mSize);
-    brightnessMenu.createFolder(brightness, bSize);
+      polarized->addParameter("Flow Rate", 0);
+      polarized->addParameter("Prim Red", 0);
+      polarized->addParameter("Prim Green", 0);
+      polarized->addParameter("Prim Blue", 0);
+      polarized->addParameter("Sec Red", 0);
+      polarized->addParameter("Sec Green", 0);
+      polarized->addParameter("Sec Blue", 0);
 
-    manager.includeFolder(&mainMenu, &musicMenu, true);
-    manager.includeFolder(&mainMenu, &lightsMenu, true);
-    manager.includeFolder(&lightsMenu, &brightnessMenu, true);
-    manager.includeFolder(&lightsMenu, &modesMenu, true);
-
-    manager.interfaceTitle = mainMenu.folderName; 
-
-    manager.begin(&mainMenu);
-
-    audio.speakerPin = audioSpeakerPin; 
-    
-    SD.begin(audioChipSelect); 
-    
-    audio.volume(audioDefaultVolume);
-}
-
-void playSong()
-{
-  String FILE_TYPE = ".wav";
+      red->addParameter("Flow Rate", 0);
+      green->addParameter("Flow Rate", 0);
+      blue->addParameter("Flow Rate", 0);
+      yellow->addParameter("Flow Rate", 0);
+      cyan->addParameter("Flow Rate", 0);
+      violet->addParameter("Flow Rate", 0);      
+    }  
+  }
   
-  if ( (manager.currentFolderOpen)->folderName == musicMenu.folderName && dial.buttonPress() && manager.currentFolderSelected != manager.BACK)
-  {     
-    String tempAudioFile_STR = manager.currentFolderSelected + FILE_TYPE;
-    
-    tempAudioFile = tempAudioFile_STR.c_str();
+  namespace songs
+  {
+    int const num_of_files = 3; 
 
-    if (manager.currentFolderSelected == prevFolderSelected)
+    file* fall_out_boy = new file("Fall Out Boy");
+    file* imagine_dragons = new file("ImagineDragons");
+    file* avicii = new file("Avicii");
+
+    file* song_files[num_of_files] = {fall_out_boy, imagine_dragons, avicii};  
+
+    void fileSetup()
     {
-       audio.disable();
-       
-       disableAudio = (!disableAudio);
-    }
-    
-    if (!disableAudio || manager.currentFolderSelected != prevFolderSelected)
-    {
-      audio.play(tempAudioFile);
-      
-      prevFolderSelected = manager.currentFolderSelected; 
+      fall_out_boy->addParameter("Song", 0);
+      imagine_dragons->addParameter("Song", 0);
+      avicii->addParameter("Song", 0);
     }
   }
-}
+
+  // assume any folder not associated with "parent" is a child folder relative to its namespace
+  namespace lights_folder
+  {
+    folder* parent_folder = new folder("[] Lights");
+
+    void folderSetup()
+    {
+      parent_folder->addFiles(RGB::RGB_files, RGB::num_of_files);
+    }
+  }
+
+  namespace music_folder
+  {
+    folder* parent_folder = new folder("[] Music");
+
+    void folderSetup()
+    {
+      parent_folder->addFiles(songs::song_files, songs::num_of_files);
+    }
+  }
+  
+  namespace base_folder
+  {
+    folder* parent_folder = new folder("**RBG Music**");
+    
+    int const num_of_child_folders = 2;
+    folder* child_folders[num_of_child_folders] = {lights_folder::parent_folder, music_folder::parent_folder};
+    
+    void folderSetup()
+    {
+      parent_folder->addFolders(child_folders, num_of_child_folders);
+    }
+  }
+
+  void setup()
+  {
+    Serial.begin(9600);    
+    audio.speakerPin = AUDIO_SPEAKER_PIN;  
+    SD.begin(AUDIO_CHIP_SELECT);
+    audio.volume(AUDIO_DEFAULT_VOLUME);
+  
+    RGB::fileSetup();
+    songs::fileSetup();
+    lights_folder::folderSetup();
+    music_folder::folderSetup();
+    base_folder::folderSetup();
+    interface.begin(base_folder::parent_folder);
+  }
+
+  void sendSerialTransmission(char* transmission)
+  {
+    Serial.write(SEND_SERIAL_ID);
+    Serial.write(transmission);
+    Serial.write(SENT_SERIAL_ID);
+  }
+
+  void RGB_Command(char* command)
+  {
+    for(int file_index = 0; file_index < lights_folder::parent_folder->getNumOfFiles(); file_index++)
+    {
+      file* current_file_ptr = lights_folder::parent_folder->getFilePTR(file_index);
+      
+      if(current_file_ptr->getFileName() == command && current_file_ptr->getFileActive())
+      {
+        sendSerialTransmission(command);
+        sendSerialTransmission(((String)current_file_ptr->getNumOfParameters()).c_str());
+
+        for(int i = 0; i < current_file_ptr->getNumOfParameters(); i++)
+        {
+          sendSerialTransmission(((String)(current_file_ptr->getParameterPTRs() + i)->getData()).c_str());
+        } 
+      }
+      else if(current_file_ptr->getFileName() != command && current_file_ptr->getFileActive())
+      {
+        current_file_ptr->setFileActive(false);
+      }
+    }
+  }
+ 
+  
+  void runAudio()
+  {
+    static String song_playing = "";
+    String FILE_TYPE = ".wav";
+
+    for(int i = 0; i < songs::num_of_files; i++)
+    {
+      if(song_playing != music_folder::parent_folder->getFilePTR(i)->getFileName() + (String)(music_folder::parent_folder->getFilePTR(i)->getParameterPTRs()->getData()) && music_folder::parent_folder->getFilePTR(i)->getFileActive())
+      {
+        audio.disable();
+        audio.play(((String)music_folder::parent_folder->getFilePTR(i)->getFileName() + (String)(music_folder::parent_folder->getFilePTR(i)->getParameterPTRs()->getData()) + FILE_TYPE).c_str());     
+        song_playing = music_folder::parent_folder->getFilePTR(i)->getFileName();
+        break;
+      }
+      else if(song_playing == music_folder::parent_folder->getFilePTR(i)->getFileName() + (String)(music_folder::parent_folder->getFilePTR(i)->getParameterPTRs()->getData()) && !(music_folder::parent_folder->getFilePTR(i)->getFileActive()))
+      {
+        audio.disable(); 
+      }
+    }
+  }
 
 void loop()
 {
-    manager.interface(); 
-
-    playSong();
-    
-    
+   interface.interface(); 
+   runAudio();
+   
+   RGB_Command("Stripes");
+   RGB_Command("Rainbow");
+   RGB_Command("Custom");
+   RGB_Command("Polarized");
+   RGB_Command("Red");
+   RGB_Command("Green");
+   RGB_Command("Blue");
+   RGB_Command("Yellow");
+   RGB_Command("Cyan");
+   RGB_Command("Violet");
 }
--- RGB-Color.ino
+++ RGB-Color.ino
@@ -1,207 +1,261 @@
 #include <Adafruit_PWMServoDriver.h>
-
 #include <Wire.h>
 
-Adafruit_PWMServoDriver RGB_DRIVER1 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver RGB_DRIVER2 = Adafruit_PWMServoDriver(0x41);
-
-int const maxDutyCycle = 30;
-int const maxDriverPWM_Cycle = 4095;
-
-void setup() 
-{
-    Serial.begin(9600);
-
-    RGB_DRIVER1.begin();
-    RGB_DRIVER2.begin();
-
-    RGB_DRIVER1.setPWMFreq(200);
-    RGB_DRIVER2.setPWMFreq(200);
-}
+Adafruit_PWMServoDriver RGB_DRIVER_1 = Adafruit_PWMServoDriver(0x40);
+Adafruit_PWMServoDriver RGB_DRIVER_2 = Adafruit_PWMServoDriver(0x41);
+int const max_duty_cycle = 30;
+int const max_driver_PWM_cycle = 4095;
 
 struct RGB_Pin
 {
-    Adafruit_PWMServoDriver* pinDriver_PTR = nullptr; 
+  Adafruit_PWMServoDriver* pin_driver_PTR = nullptr;
+  int pin;
 
-    int pin;
-  
-   RGB_Pin(Adafruit_PWMServoDriver* tempPinDriver_PTR, int tempPin)
-   {
-        pinDriver_PTR = tempPinDriver_PTR;
+  RGB_Pin(Adafruit_PWMServoDriver* pin_driver_PTR, int pin)
+  {
+    this->pin_driver_PTR = pin_driver_PTR;
+    this->pin = pin;
 
-        pin = tempPin; 
+    if (pin_driver_PTR == nullptr)
+    {
+      pinMode(pin, OUTPUT);
+    }
+  }
+};
 
-        if (tempPinDriver_PTR == nullptr)
-        {
-            pinMode(pin, OUTPUT);
-        }
-   }
+struct Cavity
+{
+  RGB_Pin* red_pin_PTR = nullptr;
+  RGB_Pin* green_pin_PTR = nullptr;
+  RGB_Pin* blue_pin_PTR = nullptr;
+
+  float pct_red, pct_green, pct_blue;
+
+  Cavity(RGB_Pin* blue_pin_PTR, RGB_Pin* green_pin_PTR, RGB_Pin* red_pin_PTR)
+  {
+    this->red_pin_PTR = red_pin_PTR;
+    this->green_pin_PTR = green_pin_PTR;
+    this->blue_pin_PTR = blue_pin_PTR;
+  }
+};
+
+class Cavities
+{
+private: public:
+
+    Cavity** cavity_PTR_PTRs = nullptr;
+    int num_of_cavities = 0;
+
+    void interfaceLEDs(Cavity* cavity_PTR)
+    {
+      if ( (cavity_PTR->red_pin_PTR)->pin_driver_PTR != nullptr)
+      {
+        ( (cavity_PTR->red_pin_PTR)->pin_driver_PTR)->setPWM( (cavity_PTR->red_pin_PTR)->pin, 0, ( (cavity_PTR->pct_red) / 100) * max_driver_PWM_cycle);
+      }
+      else if ( (cavity_PTR->red_pin_PTR)->pin_driver_PTR == nullptr)
+      {
+        analogWrite( (cavity_PTR->red_pin_PTR)->pin, ( (cavity_PTR->pct_red) / 100) * max_duty_cycle);
+      }
+
+      if ( (cavity_PTR->green_pin_PTR)->pin_driver_PTR != nullptr)
+      {
+        ( (cavity_PTR->green_pin_PTR)->pin_driver_PTR)->setPWM( (cavity_PTR->green_pin_PTR)->pin, 0, ( (cavity_PTR->pct_green) / 100) * max_driver_PWM_cycle);
+      }
+      else if ( (cavity_PTR->green_pin_PTR)->pin_driver_PTR == nullptr)
+      {
+        analogWrite( (cavity_PTR->green_pin_PTR)->pin, ( (cavity_PTR->pct_green) / 100) * max_duty_cycle);
+      }
+
+      if ( (cavity_PTR->blue_pin_PTR)->pin_driver_PTR != nullptr)
+      {
+        ( (cavity_PTR->blue_pin_PTR)->pin_driver_PTR)->setPWM( (cavity_PTR->blue_pin_PTR)->pin, 0, ( (cavity_PTR->pct_blue) / 100) * max_driver_PWM_cycle);
+      }
+      else if ( (cavity_PTR->blue_pin_PTR)->pin_driver_PTR == nullptr)
+      {
+        analogWrite( (cavity_PTR->blue_pin_PTR)->pin, ( (cavity_PTR->pct_blue) / 100) * max_duty_cycle);
+      }
+    }
+
+  public:
+
+    Cavities(Cavity* cavity_PTRs[], int num_of_cavities)
+    {
+      cavity_PTR_PTRs = new Cavity*[num_of_cavities];
+      *cavity_PTR_PTRs = cavity_PTRs[0];
+      this->num_of_cavities = num_of_cavities;
+    }
+
+    ~Cavities()
+    {
+      delete[] cavity_PTR_PTRs;
+      cavity_PTR_PTRs = nullptr;
+    }
+
+    void interfaceCavity(Cavity* cavity_PTR, float pct_red, float pct_green, float pct_blue)
+    {
+      cavity_PTR->pct_red = pct_red;
+      cavity_PTR->pct_green = pct_green;
+      cavity_PTR->pct_blue = pct_blue;
+    }
+
+    void interfaceAllCavities(float pct_red, float pct_green, float pct_blue)
+    {
+      for (int i = 0; i < num_of_cavities; i++)
+      {
+        interfaceCavity(*(cavity_PTR_PTRs + i), pct_red, pct_green, pct_blue);
+      }
+    }
+
+    void updateLyricNote()
+    {
+      for (int i = 0; i < num_of_cavities; i++)
+      {
+        interfaceLEDs(*(cavity_PTR_PTRs + i));
+      }
+    }
+};
+
+struct Hue
+{
+  float pct_red;
+  float pct_green;
+  float pct_blue;
+
+  Hue(float pct_red, float pct_green, float pct_blue)
+  {
+    this->pct_red = pct_red;
+    this->pct_green = pct_green;
+    this->pct_blue = pct_blue;
+  }
 };
 
 //Servo Driver #1 Pins
-RGB_Pin p0 = RGB_Pin(&RGB_DRIVER1, 0); RGB_Pin p1 = RGB_Pin(&RGB_DRIVER1, 1); RGB_Pin p2 = RGB_Pin(&RGB_DRIVER1, 2);
-RGB_Pin p3 = RGB_Pin(&RGB_DRIVER1, 3); RGB_Pin p4 = RGB_Pin(&RGB_DRIVER1, 4); RGB_Pin p5 = RGB_Pin(&RGB_DRIVER1, 5);
-RGB_Pin p6 = RGB_Pin(&RGB_DRIVER1, 6); RGB_Pin p7 = RGB_Pin(&RGB_DRIVER1, 7); RGB_Pin p8 = RGB_Pin(&RGB_DRIVER1, 8);
-RGB_Pin p9 = RGB_Pin(&RGB_DRIVER1, 9); RGB_Pin p10 = RGB_Pin(&RGB_DRIVER1, 10); RGB_Pin p11 = RGB_Pin(&RGB_DRIVER1, 11);
-RGB_Pin p12 = RGB_Pin(&RGB_DRIVER1, 12); RGB_Pin p13 = RGB_Pin(&RGB_DRIVER1, 13); RGB_Pin p14 = RGB_Pin(&RGB_DRIVER1, 14);
-RGB_Pin p15 = RGB_Pin(&RGB_DRIVER1, 15);
+RGB_Pin p0 = RGB_Pin(&RGB_DRIVER_1, 0); RGB_Pin p1 = RGB_Pin(&RGB_DRIVER_1, 1); RGB_Pin p2 = RGB_Pin(&RGB_DRIVER_1, 2);
+RGB_Pin p3 = RGB_Pin(&RGB_DRIVER_1, 3); RGB_Pin p4 = RGB_Pin(&RGB_DRIVER_1, 4); RGB_Pin p5 = RGB_Pin(&RGB_DRIVER_1, 5);
+RGB_Pin p6 = RGB_Pin(&RGB_DRIVER_1, 6); RGB_Pin p7 = RGB_Pin(&RGB_DRIVER_1, 7); RGB_Pin p8 = RGB_Pin(&RGB_DRIVER_1, 8);
+RGB_Pin p9 = RGB_Pin(&RGB_DRIVER_1, 9); RGB_Pin p10 = RGB_Pin(&RGB_DRIVER_1, 10); RGB_Pin p11 = RGB_Pin(&RGB_DRIVER_1, 11);
+RGB_Pin p12 = RGB_Pin(&RGB_DRIVER_1, 12); RGB_Pin p13 = RGB_Pin(&RGB_DRIVER_1, 13); RGB_Pin p14 = RGB_Pin(&RGB_DRIVER_1, 14);
+RGB_Pin p15 = RGB_Pin(&RGB_DRIVER_1, 15);
 
 //Servo Driver #2 Pins
-RGB_Pin p16 = RGB_Pin(&RGB_DRIVER2, 0); RGB_Pin p17 = RGB_Pin(&RGB_DRIVER2, 1); RGB_Pin p18 = RGB_Pin(&RGB_DRIVER2, 2);
-RGB_Pin p19 = RGB_Pin(&RGB_DRIVER2, 3); RGB_Pin p20 = RGB_Pin(&RGB_DRIVER2, 4); RGB_Pin p21 = RGB_Pin(&RGB_DRIVER2, 5);
-RGB_Pin p22 = RGB_Pin(&RGB_DRIVER2, 6); RGB_Pin p23 = RGB_Pin(&RGB_DRIVER2, 7); RGB_Pin p24 = RGB_Pin(&RGB_DRIVER2, 8);
-RGB_Pin p25 = RGB_Pin(&RGB_DRIVER2, 9); RGB_Pin p26 = RGB_Pin(&RGB_DRIVER2, 10); RGB_Pin p27 = RGB_Pin(&RGB_DRIVER2, 11);
-RGB_Pin p28 = RGB_Pin(&RGB_DRIVER2, 12); RGB_Pin p29 = RGB_Pin(&RGB_DRIVER2, 13); RGB_Pin p30 = RGB_Pin(&RGB_DRIVER2, 14);
-RGB_Pin p31 = RGB_Pin(&RGB_DRIVER2, 15);
+RGB_Pin p16 = RGB_Pin(&RGB_DRIVER_2, 0); RGB_Pin p17 = RGB_Pin(&RGB_DRIVER_2, 1); RGB_Pin p18 = RGB_Pin(&RGB_DRIVER_2, 2);
+RGB_Pin p19 = RGB_Pin(&RGB_DRIVER_2, 3); RGB_Pin p20 = RGB_Pin(&RGB_DRIVER_2, 4); RGB_Pin p21 = RGB_Pin(&RGB_DRIVER_2, 5);
+RGB_Pin p22 = RGB_Pin(&RGB_DRIVER_2, 6); RGB_Pin p23 = RGB_Pin(&RGB_DRIVER_2, 7); RGB_Pin p24 = RGB_Pin(&RGB_DRIVER_2, 8);
+RGB_Pin p25 = RGB_Pin(&RGB_DRIVER_2, 9); RGB_Pin p26 = RGB_Pin(&RGB_DRIVER_2, 10); RGB_Pin p27 = RGB_Pin(&RGB_DRIVER_2, 11);
+RGB_Pin p28 = RGB_Pin(&RGB_DRIVER_2, 12); RGB_Pin p29 = RGB_Pin(&RGB_DRIVER_2, 13); RGB_Pin p30 = RGB_Pin(&RGB_DRIVER_2, 14);
+RGB_Pin p31 = RGB_Pin(&RGB_DRIVER_2, 15);
 
 //Pins Without Driver (Arduino PWM Pins)
 RGB_Pin p32 = RGB_Pin(nullptr, 3);
 RGB_Pin p33 = RGB_Pin(nullptr, 5); RGB_Pin p34 = RGB_Pin(nullptr, 6); RGB_Pin p35 = RGB_Pin(nullptr, 9);
-
-struct Cavity
-{
-    RGB_Pin* redPin_PTR = nullptr;
-    RGB_Pin* greenPin_PTR = nullptr;
-    RGB_Pin* bluePin_PTR = nullptr;
-
-    float pctRed, pctGreen, pctBlue; 
-
-    Cavity(RGB_Pin* tempBluePin_PTR, RGB_Pin* tempGreenPin_PTR, RGB_Pin* tempRedPin_PTR)
-    {
-        redPin_PTR = tempRedPin_PTR;
-        greenPin_PTR = tempGreenPin_PTR;
-        bluePin_PTR = tempBluePin_PTR;        
-    }
-};
 
 Cavity c0 = Cavity(&p0, &p1, &p2); Cavity c1 = Cavity(&p3, &p4, &p5); Cavity c2 = Cavity(&p6, &p7, &p8);
 Cavity c3 = Cavity(&p9, &p10, &p11); Cavity c4 = Cavity(&p12, &p13, &p14); Cavity c5 = Cavity(&p15, &p16, &p17);
 Cavity c6 = Cavity(&p18, &p19, &p20); Cavity c7 = Cavity(&p21, &p22, &p23); Cavity c8 = Cavity(&p24, &p25, &p26);
 Cavity c9 = Cavity(&p27, &p28, &p29); Cavity c10 = Cavity(&p30, &p31, &p32); Cavity c11 = Cavity(&p33, &p34, &p35);
 
-Cavity* listOfCavities[12] = {&c0, &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9, &c10, &c11};
+Cavity* cavity_PTRs[12] = {&c0, &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9, &c10, &c11};
+Cavities lyricNote = Cavities(cavity_PTRs, 12);
 
-class Cavities
+Hue red = Hue(100, 0, 0), purple = Hue(100, 0, 100), green = Hue(0, 100, 0), cyan = Hue(0, 100, 100), blue = Hue(0, 0, 100), yellow = Hue(100, 100, 0), orange = Hue(100, 15, 0);
+Hue hue[7] = {red, purple, green, cyan, blue, yellow, orange};
+
+// If the slave arduino is sent this value, it knows that the following data is the command
+char const SEND_SERIAL_ID = (char)58;
+// If the slave arduino is sent this value, it knows that the entire command has been sent
+char const SENT_SERIAL_ID = (char)59;
+
+namespace RGB_command
 {
-    private:public:
+String command = "";
+int num_of_parameters = 0;
+int* parameter_ptrs = nullptr;
+}
 
-    Cavity** listOfCavities_PTR = nullptr;
-
-    int amountOfCavities;
-    
-    void interfaceLEDs(Cavity* tempCavity_PTR)
-    {
-        if ( (tempCavity_PTR->redPin_PTR)->pinDriver_PTR != nullptr)
-        { 
-            ( (tempCavity_PTR->redPin_PTR)->pinDriver_PTR)->setPWM( (tempCavity_PTR->redPin_PTR)->pin, 0, ( (tempCavity_PTR->pctRed)/100) * maxDriverPWM_Cycle );
-        }
-        else if ( (tempCavity_PTR->redPin_PTR)->pinDriver_PTR == nullptr)
-        {
-            analogWrite( (tempCavity_PTR->redPin_PTR)->pin, ( (tempCavity_PTR->pctRed)/100) * maxDutyCycle);
-        }
-
-        if ( (tempCavity_PTR->greenPin_PTR)->pinDriver_PTR != nullptr)
-        {
-            ( (tempCavity_PTR->greenPin_PTR)->pinDriver_PTR)->setPWM( (tempCavity_PTR->greenPin_PTR)->pin, 0, ( (tempCavity_PTR->pctGreen)/100) * maxDriverPWM_Cycle );
-        }
-        else if ( (tempCavity_PTR->greenPin_PTR)->pinDriver_PTR == nullptr)
-        {
-            analogWrite( (tempCavity_PTR->greenPin_PTR)->pin, ( (tempCavity_PTR->pctGreen)/100) * maxDutyCycle);
-        }
-
-        if ( (tempCavity_PTR->bluePin_PTR)->pinDriver_PTR != nullptr)
-        {
-            ( (tempCavity_PTR->bluePin_PTR)->pinDriver_PTR)->setPWM( (tempCavity_PTR->bluePin_PTR)->pin, 0, ( (tempCavity_PTR->pctBlue)/100) * maxDriverPWM_Cycle );
-        }
-        else if ( (tempCavity_PTR->bluePin_PTR)->pinDriver_PTR == nullptr)
-        {
-            analogWrite( (tempCavity_PTR->bluePin_PTR)->pin, ( (tempCavity_PTR->pctBlue)/100) * maxDutyCycle);
-        } 
-    }
-
-    public:
-
-    Cavities(Cavity* tempListOfCavity_PTRs[], int tempAmountOfCavities)
-    {
-        listOfCavities_PTR = new Cavity*[tempAmountOfCavities];
-        
-        for (int i = 0; i < tempAmountOfCavities; i++)
-        {
-            *(listOfCavities_PTR + i) = tempListOfCavity_PTRs[i];
-        }
-
-        amountOfCavities = tempAmountOfCavities;
-    }
-
-    ~Cavities()
-    {
-      delete[] listOfCavities_PTR;
-
-      listOfCavities_PTR = nullptr;
-    }
-  
-    void interfaceCavity(Cavity* tempCavity_PTR, float temp_pctRed, float temp_pctGreen, float temp_pctBlue )
-    {
-       tempCavity_PTR->pctRed = temp_pctRed;
-       tempCavity_PTR->pctGreen = temp_pctGreen;
-       tempCavity_PTR->pctBlue = temp_pctBlue;
-    }
-
-    void interfaceAllCavities(float temp_pctRed, float temp_pctGreen, float temp_pctBlue)
-    {
-        for (int i = 0; i < amountOfCavities; i++)
-        {
-            interfaceCavity(*(listOfCavities_PTR + i), temp_pctRed, temp_pctGreen, temp_pctBlue);
-        }
-    }
-
-    void updateLyricNote()
-    {
-         for (int i = 0; i < amountOfCavities; i++)
-         {
-             interfaceLEDs(*(listOfCavities_PTR + i) );
-         }
-    }
-};
-
-Cavities lyricNote = Cavities(listOfCavities, 12);
-bool ONCE = true;
-void loop() 
+void setup()
 {
-  if (ONCE)
+  Serial.begin(9600);
+  RGB_DRIVER_1.begin();
+  RGB_DRIVER_2.begin();
+  RGB_DRIVER_1.setPWMFreq(200);
+  RGB_DRIVER_2.setPWMFreq(200);
+  pinMode(A3, OUTPUT);
+  digitalWrite(A3, HIGH);
+}
+
+bool transmit(char byte_sent)
+{
+  static bool transmitting = false;
+
+  if (byte_sent == SEND_SERIAL_ID && !transmitting)
   {
-    for (int i = 0; i < 100; i++)
-    {
-       lyricNote.interfaceAllCavities(i, 0, 0);
-       lyricNote.updateLyricNote();
-
-       delay(3);
-    }
-
-    ONCE = false;
+    transmitting = true;
   }
-    
-    for (int i = 100; i > 0; i--)
-    {
-       lyricNote.interfaceAllCavities(i, (100-i), 0);
-       lyricNote.updateLyricNote();
+  else if (byte_sent == SENT_SERIAL_ID && transmitting)
+  {
+    transmitting = false;
+  }
 
-       delay(3);
-    }
-  for (int i = 100; i > 0; i--)
-    {
-       lyricNote.interfaceAllCavities(0, i, (100-i) );
-       lyricNote.updateLyricNote();
+  return transmitting;
+}
 
-       delay(3);
-    }
-    for (int i = 100; i > 0; i--)
-    {
-       lyricNote.interfaceAllCavities( (100-i) , 0, i);
-       lyricNote.updateLyricNote();
+char getChar()
+{
+  if (Serial.available())
+  {
+    return (char)Serial.read();
+  }
+}
 
-       delay(3);
+String getSerialTransmission()
+{
+  String transmission = "";
+  char character = getChar();
+
+  while (transmit(character))
+  {
+    if (character != NULL && character != SEND_SERIAL_ID && character != SENT_SERIAL_ID)
+    {
+      transmission += character;
     }
+
+    character = getChar();
+  }
+
+  return transmission;
+}
+
+void serialEvent()
+{
+  delay(100);
   
-   
+  if (Serial && Serial.available())
+  {
+    RGB_command::command = getSerialTransmission();
+    RGB_command::num_of_parameters = getSerialTransmission().toInt();
+
+    if (RGB_command::parameter_ptrs != nullptr)
+    {
+      delete[] RGB_command::parameter_ptrs;
+    }
+
+    RGB_command::parameter_ptrs = new int[RGB_command::num_of_parameters];
+
+    for (int i = 0; i < RGB_command::num_of_parameters; i++)
+    {
+      *(RGB_command::parameter_ptrs + i) = getSerialTransmission().toInt();
+    }
+
+    Serial.print("command: "); Serial.println(RGB_command::command);
+    for (int i = 0; i < RGB_command::num_of_parameters; i++)
+    {
+      Serial.print("parameter: "); Serial.println(*(RGB_command::parameter_ptrs + i));
+    }
+  }
+}
+
+void loop()
+{
+  delay(500);
+
 }
